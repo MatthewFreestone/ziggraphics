fbbbf935be0a2e570881cd575dc74def1645167c
Matthew Freestone <matthewfreestone1593@gmail.com>
2025-06-16 01:16:26 -0500

    Added color to background

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,17 @@ const c = @cImport({
     @cInclude("glad/glad.h");
     @cInclude("GLFW/glfw3.h");
 });
+const std = @import("std");
+
+fn framebuffer_size_callback(_: ?*c.GLFWwindow, width: c_int, height: c_int) callconv(.c) void {
+    c.glViewport(0, 0, width, height);
+}
+
+fn processInputs(window: *c.GLFWwindow) void {
+    if (c.glfwGetKey(window, c.GLFW_KEY_ESCAPE) == c.GLFW_PRESS)
+        c.glfwSetWindowShouldClose(window, 1);
+}
+
 pub fn main() !void {
     // const loader = &c.glfwGetProcAddress;
     // const loader: ?*const fn (procname: [*c]const u8) callconv(.c) ?*const fn () callconv(.c) void = &c.glfwGetProcAddress;
@@ -19,12 +30,15 @@ pub fn main() !void {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
 
-    const window: ?*c.GLFWwindow = c.glfwCreateWindow(800, 600, "Sample Window", null, null);
-    if (window == null) {
+    const width = 800;
+    const height = 600;
+    const maybeWindow: ?*c.GLFWwindow = c.glfwCreateWindow(width, height, "Sample Window", null, null);
+    if (maybeWindow == null) {
         std.debug.print("Failed to create window\n", .{});
         c.glfwTerminate();
         return;
     }
+    const window: *c.GLFWwindow = maybeWindow.?;
     c.glfwMakeContextCurrent(window);
 
     const get_proc_address: c.GLADloadproc = @ptrCast(&c.glfwGetProcAddress);
@@ -34,7 +48,16 @@ pub fn main() !void {
         return;
     }
 
-    c.glViewport(0, 0, 800, 600);
-}
+    c.glViewport(0, 0, width, height);
+    _ = c.glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    while (c.glfwWindowShouldClose(window) != 1) {
+        processInputs(window);
 
-const std = @import("std");
+        c.glClearColor(0.2, 0.3, 0.3, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        c.glfwPollEvents();
+        c.glfwSwapBuffers(window);
+    }
+    c.glfwTerminate();
+}
